a54903986c90f816deaec97e1bd5f3073378e1ac
Sandra Colomer <3496sandra@gmail.com>
2026-04-13 19:26:04 +0200

    conf

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
-import dns from "node:dns";
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
